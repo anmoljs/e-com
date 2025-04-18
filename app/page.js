0b9347ce7d7b4e4055cfc29668623efe8141b4c9
anmoljs <anmolasim.awan@gamil.com>
2025-04-17 22:34:22 -0700

    page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
-import './globals.css';
-import React from "react";
+
+
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import ProductRange from './components/ProductRange';
